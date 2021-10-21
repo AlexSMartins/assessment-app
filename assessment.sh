edd3022375a90a93e86b5cb187ffbc6bf35cc004
AlexSMartins <85200255+AlexSMartins@users.noreply.github.com>
2021-10-21 19:53:39 -0300

    Update assessment.sh

--- assessment.sh
+++ assessment.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 > assessment.txt
 date +%d-%m-%y >> assessment.txt
-echo
-echo
 echo "Informacoes da Distro utilizada $(hostname -i):" >> assessment.txt
 cat /etc/*-release >> assessment.txt
 echo "================================" >> assessment.txt
